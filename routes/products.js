30bb65ee3ece2ed948bf173cf48d2cc49854065e
David Mercado Juanes <davejuanes>
2022-09-14 22:31:47 -0400

    VALIDATOR HANDLER CALIDADOR DE DATOS

--- routes/products.js
+++ routes/products.js
@@ -14,29 +14,31 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filterr');
 });
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = await service.findOne(id);
-  res.json(product);
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await service.findOne(id);
+    res.json(product);
+  } catch (error) {
+    next(error)
+  }
 });
 
 
 router.post('/',  async(req, res) => {
   const body = req.body;
   const newProduct = await service.create(body);
-  res.status(201).json(newProduct)
+  res.status(201).json(newProduct);
 })
 
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
     const body = req.body;
     const product = await service.update(id, body);
     res.json(product)
   } catch (error) {
-    res.status(404).json({
-      message: error.message
-    });
+    next(error)
   }
 
 })
